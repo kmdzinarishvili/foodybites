966ec57c537c348569146fd5a4d02ec4499e81d6
keti <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-03-30 11:26:11 +0400

    added styling

--- components/SectionViews/FriendView.js
+++ components/SectionViews/FriendView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Image} from 'react-native';
+import {w,h} from '../../proportion';
 
 const FriendView = ({image}) => {
     return(
             <Image
-            style={{borderRadius:100, width:100, height:100}}
+            style={{borderRadius:100, width:130*w, height:130*h, marginHorizontal:22*w,
+                    marginTop:54*h}}
             source={{
                 uri:image
             }}
