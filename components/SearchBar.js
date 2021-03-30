99f4a84645beb264e82167295af7270dd3208872
keti <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-03-30 11:25:27 +0400

    styling/sizing

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Image, TextInput, StyleSheet} from 'react-native'
+import {View, Image, TextInput, StyleSheet} from 'react-native';
+import {w,h} from '../proportion';
 
 const SearchBar = (searchTerm, setSearchTerm) => {
     return(
@@ -21,22 +22,25 @@ const SearchBar = (searchTerm, setSearchTerm) => {
 
 const styles = StyleSheet.create({
     SectionStyle: {
+        marginTop:20,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: '#fff',
         borderWidth: .5,
-        borderColor: '#000',
-        height: 40,
+        borderColor: '#E8E8E8',
+        height: 154*h,
         borderRadius: 5 ,
+        paddingHorizontal:54*w,
     },
     
     ImageStyle: {
-        padding: 10,
+        marginVertical:10,
+        padding: 0,
         margin: 5,
-        height: 25,
-        width: 25,
-        resizeMode : 'stretch',
+        height: 50*h,
+        width: 50*h,
+        // resizeMode : 'stretch',
         alignItems: 'center'
     },
     
