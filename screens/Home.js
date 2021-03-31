a0494002430fbae4ca49472b9f70b47d8fc1d804
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-03-31 14:04:42 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -4,32 +4,14 @@ import {View, Text, Dimensions, FlatList, Image} from 'react-native';
 
 import SafeView from '../components/SafeView';
 import SearchBar from '../components/SearchBar';
-import HomeSection from '../components/HomeSection';
 import FooterNavigation from '../components/FooterNavigation';
 
+
 import RestSec from '../components/Sections/RestSec';
-import FriendView from '../components/SectionViews/FriendView';
-import CatView from '../components/SectionViews/CatView';
 import CatSec from '../components/Sections/CatSec';
 import FriendSec from '../components/Sections/FriendSec';
 
 const Home = () => {
-    const [categories, setCategories] = useState();
-    const [friends, setFriends] = useState();
-
-    const fetch_friend_pictures= async () =>{
-        const result = await fetch(
-            'https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=profile'
-        ).then(res=>res.json()).then(res=>res['results'])
-        .then(json => {setFriends(json)})
-        .catch((error) => {
-            throw error;
-        });
-    }
-    useEffect(() =>{
-        // fetch_restaurant_pictures();
-        fetch_friend_pictures();
-    }, [] );
     return (
         <SafeView >
             <View style={{
@@ -42,23 +24,7 @@ const Home = () => {
                 <SearchBar />
                 <RestSec/>
                 <CatSec/>
-                    {/* <HomeSection title='Friends' number={56}>    
-                    <FlatList
-                        showsHorizontalScrollIndicator={false}
-
-                        data={friends}
-                        horizontal={true}
-                        renderItem={({item})=>{
-                            return (
-                            <FriendView image={item['urls']['regular']}
-                            />
-                            )
-                       
-                        }}
-                        keyExtractor={(item) => item.id}
-                    />
-                    </HomeSection>
-                     */}
+              
                 <FriendSec/>
            </View>
            <FooterNavigation/>
