e671a42456b92a8bbb9cf55ceebe6aaeeb1d9625
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-03-31 14:47:45 +0400

    padding updated

---
+++ screens/RestaurantPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+import SafeView from '../components/SafeView';
+
+
+const RestaurantPage = () =>{
+    return(
+        <SafeView>
+            <Text>
+                Restaurant Page 
+            </Text>
+        </SafeView>
+    );
+
+}
+
+export default RestaurantPage;
