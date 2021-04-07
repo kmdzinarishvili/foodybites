3fe2de4d06a8b92c58099d32abf35007ad24329d
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-07 17:52:20 +0400

    Update Filter.js

--- screens/Filter.js
+++ screens/Filter.js
@@ -3,8 +3,15 @@ import {View, Text, FlatList} from 'react-native';
 import { h, w } from '../proportion';
 
 
-const Category = (country) =>{
-    return(<View style={{width:300*w, height:133*h}}><Text>{country}</Text></View>)
+const Category = ({country, pressed}) =>{
+    return(<View style={{width:300*w, height:133*h,
+            borderColor:"rgba(112, 112, 112, 0.4)",
+            borderRadius:23*w,
+            borderWidth:1*w,
+            justifyContent:'center',
+            alignItems:'center',margin:21*w}}>
+        <Text >{country}</Text>
+        </View>)
 
 }
 
@@ -14,10 +21,14 @@ const Filter = () =>{
     const countries = ['Italian', 'Chinese', 'Mexican',
                         'Thai', 'Arabian', 'Indian',
                         'American', 'Korean', 'European']
-    return (<View style={{backgroundColor:'#FAFAFA', height:'100%'}}>
+    return (<View style={{backgroundColor:'#FAFAFA', height:'100%',
+                padding:"4%",
+                flexDirection:'column',
+                alignItems:'center'
+                }}>
         <Text>Select Category</Text>
         <View style={{flexDirection:'row', flexWrap:true}}>
-            {countries.map((c)=> Category(c))}
+            {countries.map((c)=> < Category country = {c} pressed={false}/>)}
         </View>
     </View>)
 }
