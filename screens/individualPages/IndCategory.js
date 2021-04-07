a86eb5f6e5e2fd75e4f98d8c04d7bc43fae0a068
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-07 19:05:12 +0400

    cleaning up code, using params for gradient

--- screens/individualPages/IndCategory.js
+++ screens/individualPages/IndCategory.js
@@ -10,35 +10,13 @@ import RestSec from '../../components/Sections/RestSec';
 import RestaurantPage from '../RestaurantPage';
 
 
-const IndCategory = ({ name='Italian', gradient}) =>{
-    const [food, setFood] = useState();
+const IndCategory = ({ name='Italian',  route}) =>{
+    const gradient = (route.params);
     const navigation = useNavigation();
-    const fetch_food= async () =>{
-        const result = await fetch(
-            'https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=food'
-        ).then(res=>res.json()).then(res=>res['results'])
-        .then(json => {setFood(json)})
-        .catch((error) => {
-            throw error;
-        });
-    }    
-    
-    useEffect(()=>{
-        fetch_food();
-    }, []);
-    return (
-        // <LinearGradient
-        //     colors={gradients.pink}
-        //     style={{width:100, height:100}}>
-        //     <ImageBackground source={
-        //     {
-        //         uri:"https://images.unsplash.com/photo-1482049016688-2d3e1b311543?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MnwyMTg3NTR8MHwxfHNlYXJjaHwxfHxmb29kfGVufDB8fHx8MTYxNzYwMTMwMQ\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=1080"
-        //     }
-        //     }
-        //     style={{width:1125*w, height:317.72*h}}>
 
-        //         <Text> Individual Category Page </Text>
-        //     </ImageBackground>
+    
+    
+    return (
         <Pressable>
         
             <ImageBackground source={
@@ -49,7 +27,7 @@ const IndCategory = ({ name='Italian', gradient}) =>{
             style={{width:1125*w, height:317.72*h}}>
           
             <LinearGradient
-            colors={gradients.pink}
+            colors={gradient}
             style={{width:'100%', height:'100%', opacity:0.85,
                     justifyContent:'center', alignItems:'center',
                     position:'absolute',
