f164cf096b3ff3441493d07a175252e9b77db071
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-08 13:10:01 +0400

    choosing category functionality working

---
+++ components/Review/Rating.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const Review = () =>{
+    return (
+        <View>
+            <Text>REview</Text>
+        </View>
+    )
+}
+
+export default Review;
--- screens/Filter.js
+++ screens/Filter.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import {View, Text, FlatList, Image} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Pressable, Image} from 'react-native';
 import { h, w } from '../proportion';
 import styles from '../styles/styles';
 
+import Rating from '../components/Review/Rating';
+import gradients from '../styles/gradients';
+import {LinearGradient} from 'expo-linear-gradient';
 
-const Category = ({country, pressed}) =>{
-    return(<View style={{width:300*w, height:133*h,
+
+const Category = ({country, id, pressedId, setPressedId }) =>{
+
+    const gradient = id%3==0? gradients.pink:id%3==1? gradients.purple:gradients.blue;
+    const pressed = pressedId ==id
+    return(
+        <Pressable onPress={()=>setPressedId(id)}><View style={{width:300*w, height:133*h,
             borderColor:"rgba(112, 112, 112, 0.4)",
             borderRadius:23*w,
             borderWidth:1*w,
             justifyContent:'center',
-            alignItems:'center',margin:21*w}}>
-        <Text style={[styles.jSemi, {fontSize:50*w, color: pressed?'#FFF':'#rgba(62, 63, 104, 0.5)'}]}>{country}</Text>
-        </View>)
+            alignItems:'center',margin:21*w
+     }}>
+
+        {pressed? <LinearGradient style={{width:'100%', height:'100%',        borderColor:"rgba(112, 112, 112, 0.4)",
+            borderRadius:23*w,
+            borderWidth:1*w,
+            justifyContent:'center',
+            alignItems:'center',margin:21*w}} colors={gradient}>
+            <Text style={[styles.jSemi, {fontSize:50*w, color:'#FFF'}]}>{country}</Text>
+        </LinearGradient>: 
+                <Text style={[styles.jSemi, {fontSize:50*w, color: '#rgba(62, 63, 104, 0.5)'}]}>{country}</Text>
+
+        }
+        </View>
+        </Pressable>
+        )
 
 }
 
@@ -22,31 +43,38 @@ const FilterTitle = ({style,text})=>{
 
 
 const Filter = ({distance}) =>{
+ 
     distance= 50;
 
-    //pressed.id 
-    //then change accordingly
+    const [pressedId, setPressedId] = useState();
+    // //pressed.id 
+    // //then change accordingly
     const countries = ['Italian', 'Chinese', 'Mexican',
                         'Thai', 'Arabian', 'Indian',
                         'American', 'Korean', 'European'];
+    
     return (<View style={{backgroundColor:'#FAFAFA', height:'100%',
                 padding:"4%",
                 flexDirection:'column',
                 alignItems:'center'
                 }}>
         <FilterTitle style={{marginTop:127*h, marginBottom:83*h}} text='Select Category'/>
-        <View style={{flexDirection:'row', flexWrap:true,  justifyContent:'space-around'}}>
-            {countries.map((c)=> < Category country = {c} pressed={false}/>)}
+
+        <View style={{flexDirection:'row', flexWrap:'wrap',  justifyContent:'space-around'}}>
+            {countries.map((c, index)=> <Category country = {c} id={index} pressedId={pressedId} setPressedId={setPressedId}
+                    />)}
         </View>
         <FilterTitle style={{marginTop:127*h, marginBottom:83*h}} text='Distance'/>
         <View style={{width:'100%'}}>
         <View style={{width: distance+'%', height: 27*h, backgroundColor:'#5663FF', borderRadius:14*w,
         borderBottomRightRadius:0, borderTopRightRadius:0, position:'absolute', left:0}}></View>
-            <Image />
+             <Image /> 
             <View style={{width: 100-distance+'%', height: 27*h, backgroundColor:'#EDEEFF', borderRadius:14*w,
         borderBottomLeftRadius:0, borderTopLeftRadius:0, position:'absolute', right:0}}></View>
         </View>
         <FilterTitle style={{marginTop:127*h, marginBottom:83*h}} text='Ratings'/>
+        
+        <Rating />
 
     </View>)
 }
