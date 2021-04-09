3786f0f5ccf31cd487cbc88da141b5ff17f77b9e
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-09 13:53:37 +0400

    styling updated

--- components/Filter/DistanceSlider.js
+++ components/Filter/DistanceSlider.js
@@ -7,11 +7,16 @@ import Animated, {
 } from 'react-native-reanimated'
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
+import {w,h} from '../../proportion';
 
 
 
-const SLIDER_WIDTH = 300;
-const KNOB_WIDTH = 70;
+const SLIDER_WIDTH = 984*w;
+const SLIDER_HEIGHT =27 *h;
+const KNOB_WIDTH = 98.11*w;
+const KNOB_HEIGHT = 144.95*h;
+const KNOB_OFFSET = 69*h;
+const BORDER_RADIUS =14;
 // const MAX_RANGE = 20;
 
 const DistanceSlider =() =>{
@@ -36,7 +41,7 @@ const DistanceSlider =() =>{
 
     const progressStyle = useAnimatedStyle(()=>{
         return{
-            width:translateX.value+KNOB_WIDTH
+            width:translateX.value+KNOB_WIDTH/2
         }
     });
     
@@ -47,8 +52,8 @@ const DistanceSlider =() =>{
         <View style={styles.slider}>
             <Animated.View style={[styles.progress, progressStyle]} />
             <PanGestureHandler onGestureEvent={onGestureEvent}>
-                <Animated.View style={[ styles.knob, scrollTransitionStyle]}>
-                        <Image style={{bottom:100}}source={require('../../imgs/dist_knob.png')}/>
+                <Animated.View style={[ scrollTransitionStyle]}>
+                        <Image style={styles.knob} source={require('../../imgs/dist_knob.png')}/>
                     </Animated.View>
             </PanGestureHandler>
         </View>
@@ -59,20 +64,22 @@ const DistanceSlider =() =>{
 
 const styles = StyleSheet.create({
     slider: {
-      height: KNOB_WIDTH,
+      height: SLIDER_HEIGHT,
       width: SLIDER_WIDTH,
-      borderRadius: KNOB_WIDTH / 2,
-      backgroundColor: '#ddd',
+      borderRadius: BORDER_RADIUS,
+      backgroundColor: '#EDEEFF',
       justifyContent: 'center',
     },
     progress: {
       ...StyleSheet.absoluteFillObject,
-      backgroundColor: '#3f51b5',
-      borderRadius: KNOB_WIDTH / 2,
+      backgroundColor: '#5663FF',
+      borderTopLeftRadius: BORDER_RADIUS,
+      borderBottomLeftRadius: BORDER_RADIUS
     },
     knob: {
-        width:0, 
-        height:0,
+        width:KNOB_WIDTH, 
+        height:KNOB_HEIGHT,
+        bottom:KNOB_OFFSET
    
     },
   })
