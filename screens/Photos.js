5f9e07f349c63648aba0f546b6b41b7f48b0fc17
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-14 14:04:58 +0400

    fixed photos

--- screens/Photos.js
+++ screens/Photos.js
@@ -2,31 +2,45 @@ import React, {useState,useEffect} from 'react';
 import {View, Image, Text, StyleSheet, ScrollView, FlatList, Pressable} from 'react-native';
 
 import {w, h} from '../proportion';
+import MasonryList from "react-native-masonry-list";
 
 const Photos = ({navigation}) =>{
 
     const [food, setFood] = useState();
     const fetch_food= async () =>{
+        let currFood = [];
         const result = await fetch(
             'https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=food'
         ).then(res=>res.json()).then(res=>res['results'])
-        .then(json => {setFood(json);
+        .then(json => {
+            currFood =(json);
         })
         .catch((error) => {
             throw error;
         });
+
+        let newRes = [];
+        await currFood.map((item)=> newRes.push({uri:item['urls']['regular']}));
+        console.log(newRes);
+        setFood(newRes);
+
     }    
     
     useEffect(()=>{
         fetch_food();
     }, []);
     return <View style={styles.container}>
-        <ScrollView>
+        {/* <ScrollView> */}
         <View style={{flexDirection:'row',
                         flexWrap:'wrap',
                         justifyContent:'space-around',
+                        height: '100%'
                         }}>
-             {food?food.map((item, index)=>{
+            
+
+            <MasonryList
+            images={food} />
+             {/* {food?food.map((item, index)=>{
                 return     <Pressable
                 key={`${index}`}
                 onPress={()=>{
@@ -37,15 +51,15 @@ const Photos = ({navigation}) =>{
                              borderRadius:22*w, marginHorizontal:45*w/2,
                              margin:24*w
                                 }}
-                        source={{uri:item['urls']['regular']}}
+                        source={item}
                         />
 
                         </Pressable>
                     }):
         
-                <Text>Loading</Text>}
+                <Text>Loading</Text>} */}
             </View>
-            </ScrollView>
+            {/* </ScrollView> */}
     </View>
 }
 
