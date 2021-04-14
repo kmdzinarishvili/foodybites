da74ed2f078dd6c1f558b363d935a5fef454a54e
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-14 12:40:20 +0400

    Update Photos.js

--- screens/Photos.js
+++ screens/Photos.js
@@ -1,11 +1,11 @@
 import React, {useState,useEffect} from 'react';
-import {View, Image, Text, StyleSheet, ScrollView, FlatList} from 'react-native';
+import {View, Image, Text, StyleSheet, ScrollView, FlatList, Pressable} from 'react-native';
 
 import SafeView from '../components/SafeView';
+import useFetch from '../hooks/useFetch';
 import {w, h} from '../proportion';
 
-const Photos = () =>{
-    
+const Photos = ({navigation}) =>{
 
     const [food, setFood] = useState();
     const fetch_food= async () =>{
@@ -13,7 +13,7 @@ const Photos = () =>{
             'https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=food'
         ).then(res=>res.json()).then(res=>res['results'])
         .then(json => {setFood(json);
-            console.warn('food loaded')})
+        })
         .catch((error) => {
             throw error;
         });
@@ -22,31 +22,18 @@ const Photos = () =>{
     useEffect(()=>{
         fetch_food();
     }, []);
-    const renderItem = ({item, index}) =>{
-
-        return(
-            <Image
-            style={{
-                width:100,
-                height:100
-            }}
-            
-            source={{
-                uri:item['urls']['regular']
-            }}/>
-        )
-    }
-    return <SafeView style={styles.container}>
+    return <View style={styles.container}>
         <ScrollView>
         <View style={{flexDirection:'row',
                         flexWrap:'wrap',
                         justifyContent:'space-around',
-                        backgroundColor:'red'}}>
+                        }}>
              {food?food.map((item, index)=>{
-                 console.log(index);
-       
-             
-            return <Image 
+                return     <Pressable
+                key={`${index}`}
+                onPress={()=>{
+                    navigation.navigate('Photo');
+                }}><Image 
                         style={{width: index===4? 650*w: index===9?989*w  :300*w, 
                             height: index===4?  650*h:index===9?678*h  : 300*h,
                              borderRadius:22*w, marginHorizontal:45*w/2,
@@ -54,12 +41,14 @@ const Photos = () =>{
                                 }}
                         source={{uri:item['urls']['regular']}}
                         />
+
+                        </Pressable>
                     }):
         
                 <Text>Loading</Text>}
             </View>
             </ScrollView>
-    </SafeView>
+    </View>
 }
 
 const styles= StyleSheet.create({
@@ -67,7 +56,8 @@ const styles= StyleSheet.create({
         width:'100%', 
         height:'100%', 
         alignItems:'center', 
-        justifyContent:'center'
+        justifyContent:'center',
+        backgroundColor:'#FAFAFA'
     }
 });
 export default Photos;
