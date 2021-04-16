ab286554be555464bad4d092e5ff45ff0771e2e4
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-16 14:55:41 +0400

    category slider WORKING yay

--- screens/individualPages/CategoryNav.js
+++ screens/individualPages/CategoryNav.js
@@ -1,35 +1,65 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import {countries} from '../../data/data';
 import IndCategory from './IndCategory';
 import useFetch from '../../hooks/useFetch';
 import gradients from '../../styles/gradients';
 
-const CategoryNav = ({route}) =>{
-const {image, gradient, name} = route.params
-const categories =  useFetch('https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=food');
+import {w,h} from '../../proportion';
 
+const CategoryNav = ({route}) =>{
+
+    const categories =  useFetch('https://api.unsplash.com/search/photos/?client_id=i3AmYBQbRiDxMi3p937gP1nTnvqdBuSeyIm_99ZQ_jE&query=food');
+    
     if (categories){
+        console.log(categories);
     return (
        <AppIntroSlider 
         renderItem={({item,index})=>{
-            return (<IndCategory image={image} gradient={gradient} name={name}/>)
+            const gradient =  index%3==0? gradients.pink:index%3==1? gradients.purple:gradients.blue;
+            return (<IndCategory image={item['urls']['regular']} gradient={gradient} name={index<countries.length?countries[index]:'German'}/>)
         }
         }
         data={categories} 
         keyExtractor={(item)=>item.id}
+        renderPagination={(active)=>{
+            const numPages = categories.length
+            console.log(active);
+            return(<View style={styles.container}>
+                {categories.map((cat,index)=><View key={`${index}`}
+                  style={[styles.page, {        width:(600*w)/numPages -20*w,
+                    opacity:index===active?1:0.5}]}/>)}
+            </View>)}
+        }
         /> 
     );
     }else{
         return (
-            <View> 
+            <View style={{height:'100%', justifyContent:'center', flexDirection:'center'}}> 
                 <Text>
-                    Loading
+                    LOADING
                 </Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignSelf:'center',
+        flexDirection:'row', 
+        width:600*w,
+        position:'absolute',
+        top:250*h
+    },
+    page:{
+        margin:10*w,
+        height: 15*w,
+        backgroundColor:'#FFF',
+        borderRadius:8*w 
+    }
+})
 
 export default CategoryNav;
--- screens/individualPages/IndCategory.js
+++ screens/individualPages/IndCategory.js
@@ -29,11 +29,6 @@ const IndCategory = ({image, gradient, name ="Italian" }) =>{
                     position:'absolute',
                      top:0}}>
                 </LinearGradient>
-                <View style={{flexDirection:'row', margin:268*w }}>
-                {[1,2,3,4,5,6,7,8,9].map((num)=><View key={num} style={{margin:10*w,width:52*w, height: 15*w, backgroundColor:'#FFF', opacity:1, borderRadius:8*w }}></View>)}
-                </View>
-
-
                 <View style={{position: 'absolute',  left: 0, right: 0, bottom: '30%', justifyContent: 'center', alignItems: 'center'}}>
                 <Text  
                     style={[styles.title, {color:"#FFF"}]}> {name} </Text>
