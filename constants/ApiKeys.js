5d8d97b64f78bd1f1d34267170817e929f18ee73
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-29 20:51:23 +0400

    adding firebase for future authentication purposes maybe?

---
+++ constants/ApiKeys.js
@@ -0,0 +1,11 @@
+export default {
+	FirebaseConfig: {
+		apiKey: 'AIzaSyD-jr0O4mDKh76tQEgPtMZXTwPLZhZ213E',
+		authDomain: 'foodybite-76050.firebaseapp.com',
+		projectId: 'foodybite-76050',
+		storageBucket: 'foodybite-76050.appspot.com',
+		messagingSenderId: '634701512625',
+		appId: '1:634701512625:web:53f77f2ec409ef38d14790',
+		measurementId: 'G-76Z6285X7T',
+	},
+};
