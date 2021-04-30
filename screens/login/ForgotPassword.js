1b7943940dcfaff7bd93d7aa1ccf473d18ac447a
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-30 12:45:35 +0400

    Create ForgotPassword.js

---
+++ screens/login/ForgotPassword.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import LoginBackground from '../../components/Login/LoginBackground';
+
+const ForgotPassword = () => {
+	return (
+		<LoginBackground>
+			<View style={{ padding: 100 }}>
+				<Text>Forgot Passwordd Page</Text>
+			</View>
+		</LoginBackground>
+	);
+};
+
+export default ForgotPassword;
