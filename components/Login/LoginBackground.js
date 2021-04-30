548296dbe29f0b5ce4ee250eae0565c3e10e55e8
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-04-30 12:46:51 +0400

    abstracting away background for login page (to be used on forgot password as well)

---
+++ components/Login/LoginBackground.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+	StyleSheet,
+	ImageBackground,
+	StatusBar,
+	Text,
+	Pressable,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { w } from '../../proportion';
+
+const LoginBackground = ({ children }) => {
+	return (
+		<ImageBackground
+			source={require('../../imgs/login/loginPicture.png')}
+			style={{ width: '100%', height: '100%' }}
+		>
+			<StatusBar barStyle="light-content" />
+			<LinearGradient
+				colors={['rgba(45,45,45,0.41)', 'rgba(8,8,8,1)']}
+				style={bgStyles.linearGradient}
+			>
+				{children}
+			</LinearGradient>
+		</ImageBackground>
+	);
+};
+
+const bgStyles = StyleSheet.create({
+	linearGradient: {
+		width: '100%',
+		height: '100%',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		paddingHorizontal: 118 * w,
+	},
+});
+export default LoginBackground;
